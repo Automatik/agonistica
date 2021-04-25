86b99fec7d6b09ec8f9a669a018d7ba0093ec1d5
e.osterhed <e.osterhed@campus.unimib.it>
2021-04-25 20:24:49 +0200

    Create reviews package

--- lib/views/players/players_view.dart
+++ lib/views/players/players_view.dart
@@ -3,13 +3,11 @@ library players_view;
 import 'package:agonistica/core/models/season_player.dart';
 import 'package:agonistica/core/shared/my_sizing_information.dart';
 import 'package:agonistica/views/players/players_view_model.dart';
-import 'package:agonistica/views/teams/player_review.dart';
+import 'package:agonistica/widgets/reviews/player_review.dart';
 import 'package:agonistica/widgets/app_bars/add_action_platform_app_bar.dart';
-import 'package:agonistica/widgets/app_bars/base_platform_app_bar.dart';
 import 'package:agonistica/widgets/popups/players_view_popup_menu.dart';
 import 'package:agonistica/widgets/scaffolds/scroll_scaffold_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 import 'package:stacked/stacked.dart';
 
